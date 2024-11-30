29d6c07d1b2c3f6dbc3abac0f596b2645cd03967
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-11-30 22:38:46 +0530

    about page

---
+++ src/Pages/About.js
@@ -0,0 +1,13 @@
+import MagaInBrief from "../Components/About-contents/MagaInBrief";
+
+function About() {
+  return (
+    <div  className="m-0 pt-2">
+     
+    <section id="about-brief" className="w-full">
+    <MagaInBrief />
+    </section>
+    </div>
+  )
+}
+export default About
