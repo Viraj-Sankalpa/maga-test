16810882cf6b18797f8676d593f9407a4e61789e
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-11-30 17:23:55 +0530

    project main changes (start)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Header from "../Components/Header/Header";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
+        <main>
         {children}
+        </main>
+        
       </body>
     </html>
   );
