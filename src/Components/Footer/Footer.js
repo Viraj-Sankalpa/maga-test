faa39ebd1514e7394668fd48728f82f8cbf172af
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-12-01 14:12:09 +0530

    footer p change

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className="bg-[#00464d] text-white text-center py-4">
         <p className="text-sm">
-          © {new Date().getFullYear()} Copyright © 2015 Maga Engineering (Pvt) Ltd. All rights reserved (Design & Development by Maga Engineering)
+           Copyright © {new Date().getFullYear()} Maga Engineering (Pvt) Ltd. All rights reserved (Design & Development by Maga Engineering)
         </p>
       </footer>
     );
