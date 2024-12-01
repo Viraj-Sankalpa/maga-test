5e3742c0284ac509387738d0015f00c73c253490
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-12-01 08:07:31 +0530

    home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+'use client'
+
+import HomeDescription from '../Components/HomePage-content/HomeDescription';
+import Slideshow from '../Components/SlideShow/Slideshow';
+
+function HomePage() {
+  return (
+    <div>
+      <Slideshow />
+      <HomeDescription />
+    </div>
+  )
+}
+
+export default HomePage;
