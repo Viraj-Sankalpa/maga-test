1c9e2320c0a473dac8b2090c58e0aa3c05d7215a
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-12-01 10:43:08 +0530

    main page ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 import HomePage from "../Pages/Home";
 import About from "../pages/About";
+import Contact from "../pages/Contact";
 import Projects from "../pages/Projects";
 export default function Home() {
   return (
-   
-      <main className="flex flex-col bacground-main row-start-2 items-center sm:items-start">
-       
-         <section id="home" className="w-full">
+    <main className="flex flex-col bacground-main row-start-2 items-center sm:items-start">
+      <section id="home" className="w-full">
         <HomePage />
       </section>
       <section id="about" className="w-full">
-      <About />
+        <About />
       </section>
       <section id="projects" className="w-full">
-      <Projects />
+        <Projects />
       </section>
-    
-      </main>
-      
+      <section id="contact" className="w-full">
+        <Contact />
+      </section>
+    </main>
   );
 }
